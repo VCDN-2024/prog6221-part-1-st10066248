9401f040cc563cecbbc50852e802738b54d61a5d
st10066248 <128744069+st10066248@users.noreply.github.com>
2024-04-16 12:07:06 +0200

    Add files via upload
    Created the relevant classes and added a blue colour to the foreground

---
+++ st10066249_POE_Part1/st10066249_POE_Part1/Ingredients.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace st10066249_POE_Part1
+{
+    internal class Ingredients
+    {
+    }
+}
--- st10066249_POE_Part1/st10066249_POE_Part1/Program.cs
+++ st10066249_POE_Part1/st10066249_POE_Part1/Program.cs
@@ -1,2 +1,12 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using st10066249_POE_Part1;
+
+ class Program
+{
+    private static void Main(string[] args)
+    {
+        Recipe recipe = new Recipe();
+        Console.ForegroundColor
+       = ConsoleColor.Blue;
+        Console.WriteLine("Enter the details for the recipe:");
+    }
+}
