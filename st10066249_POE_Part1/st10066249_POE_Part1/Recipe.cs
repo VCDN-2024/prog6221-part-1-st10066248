3b7b9a8760caec384ef6d4ef18875382a7d89d76
st10066248 <128744069+st10066248@users.noreply.github.com>
2024-04-16 12:18:42 +0200

    Add files via upload
    Ingredient class coded with errors

--- st10066249_POE_Part1/st10066249_POE_Part1/Recipe.cs
+++ st10066249_POE_Part1/st10066249_POE_Part1/Recipe.cs
@@ -8,5 +8,19 @@ namespace st10066249_POE_Part1
 {
     internal class Recipe
     {
+        public List<Ingredient> Ingredients { get; set; }
+        public List<RecipeStep> Steps { get; set; }
+
+        public Recipe()
+        {
+            Ingredients = new List<Ingredient>();
+            Steps = new List<RecipeStep>();
+        }
+
+        public void AddIngredient(string name, double quantity, string unit)
+        {
+            Ingredients.Add(new Ingredient { Name = name, Quantity = quantity, Unit = unit });
+        }
+
     }
 }
